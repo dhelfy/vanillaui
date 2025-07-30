9522f96d725889db14900badea695db885e94f10
dhelfy <dhcode4business@gmail.com>
2025-07-30 19:12:15 +0500

    results in search now are links; visual improvements in search

--- src/shared/Searchbar/Searchbar.tsx
+++ src/shared/Searchbar/Searchbar.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import { ButtonHTMLAttributes, FC, useEffect, useState } from "react";
+import { ButtonHTMLAttributes, FC, useEffect, useRef, useState } from "react";
 import styles from "./Searchbar.module.css";
 import Image from "next/image";
 import Modal from "../Modal/Modal";
 import CstmInput from "../CstmInput/CstmInput";
 import Fuse from 'fuse.js'
 import { fetchComponents } from "@/API/componentsAPI";
+import Link from "next/link";
 
 interface props extends ButtonHTMLAttributes<HTMLButtonElement> { }
 
 const Searchbar: FC<props> = () => {
+    const input = useRef<HTMLInputElement>(null)
     const [isOpen, setIsOpen] = useState(false)
     const [query, setQuery] = useState("")
     const [data, setData] = useState<any[]>([])
@@ -30,6 +32,13 @@ const Searchbar: FC<props> = () => {
         load()
     }, [])
 
+    const handleInputRef = (el: HTMLInputElement | null) => {
+        input.current = el
+        if (el) {
+            el.focus()
+        }
+    }
+
     return (
         <>
             <button className={styles.searchBar} onClick={() => setIsOpen(true)}>
@@ -43,16 +52,38 @@ const Searchbar: FC<props> = () => {
                 Search...
             </button>
 
-            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+            <Modal isOpen={isOpen} onClose={() => {
+                setIsOpen(false)
+                setQuery("")
+            }}>
                 <CstmInput
                     placeholder="Search..."
                     onChange={(e) => setQuery(e.currentTarget.value)}
                     value={query}
+                    ref={handleInputRef}
                 />
-                <div>
-                    {result.map((component) => {
-                        return <div key={component.item.id}>{component.item.name}</div>
-                    })}
+                <div className={styles.results}>
+                    Results:
+                    {
+                        result.length ?
+                            result.map((component) => {
+                                return (
+
+                                        <Link 
+                                            key={component.item.id} 
+                                            className={styles.result}
+                                            href={`/components/${component.item.name}/${component.item.id}`}
+                                            onClick={() => {
+                                                setIsOpen(false)
+                                                setQuery("")
+                                            }}
+                                        >
+                                            {component.item.name}
+                                        </Link>
+                                )
+                            }) :
+                        <div className={styles.noResults}>No results found 😥</div>
+                    }
                 </div>
             </Modal>
         </>
